fc9c73e89ed9175354a5bd5e042255d0a266274b
凌羿文 <adt113111@gm.ntcu.edu.tw>
2026-05-22 21:00:20 +0800

    added gitignore added DataOrganization.R and generated healthcare_dataset.csv

---
+++ DataOrganization.R
@@ -0,0 +1,16 @@
+#read the files and organizes them to 1 csv file  
+org <- function(){ 
+  patients   <- read.table("PatientCorePopulatedTable.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  admissions <- read.table("AdmissionsCorePopulatedTable.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  diagnoses  <- read.table("AdmissionsDiagnosesCorePopulatedTable.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  labs <- read.table("LabsCorePopulatedTable.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  #merge data based on ID
+  df_step1 <- merge(patients, admissions, by = "PatientID")
+  df_step2 <- merge(df_step1, diagnoses, by = c("PatientID", "AdmissionID"))
+  master_data <- merge(df_step2, labs, by = c("PatientID", "AdmissionID"), all.x = TRUE)
+  master_data <- master_data[order(master_data$PatientID, 
+                                   master_data$AdmissionStartDate, 
+                                   master_data$LabDateTime), ]
+  #turn it into csv file
+  write.csv(master_data, file = "Healthcare_DATA.csv", row.names = FALSE)
+}
